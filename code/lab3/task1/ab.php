57b6ac1b95d2685216a1ecb47f7f395739f23330
Aberone <aberone2029@mail.ru>
2022-02-20 15:26:13 +0200

    Lab 3 — Task 1 — b

--- code/lab3/task1/ab.php
+++ code/lab3/task1/ab.php
@@ -1,8 +1,21 @@
 <?php
-$str = 'ahb acb aeb aeeb adcb axeb';
+$str = "ahb acb aeb aeeb adcb axeb";
 $matches = [];
 preg_match_all("/a..b/", $str, $matches);
 echo "Задание 1-a. Найденные совпадения:";
 foreach ($matches[0] as $match) {
     echo " " . $match;
 }
+echo "<br>";
+
+
+$str = "a1b2c3";
+$str_cubes = preg_replace_callback(
+    "/[0-9]+/",
+    function (array $match)
+    {
+        return ((int)($match[0]) ** 3)."";
+    },
+    $str
+);
+echo "Задание 1-b. Изменённая строка: " . $str_cubes . "<br>";
